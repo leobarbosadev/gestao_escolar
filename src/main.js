dd358cceb0188a8342273fead4b993aa8d7bd076
leobarbosadev <leobarbosajf@hotmail.com>
2025-07-11 20:39:16 -0300

    criação de arquivos js

---
+++ src/main.js
@@ -0,0 +1,7 @@
+const nome = document.getElementById('nome_completo');
+const frequencia = document.getElementById('frequencia');
+const nota1 = Number(document.getElementById('nota1'));
+const nota2 = Number(document.getElementById('nota2'));
+const nota3 = Number(document.getElementById('nota3'));
+const nota4 = Number(document.getElementById('nota4'));
+
