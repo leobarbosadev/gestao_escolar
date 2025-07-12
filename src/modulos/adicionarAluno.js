5283f637bdbb79bf9190ca2e41e4b31a6c82663d
colynascimento <colymsnascimento@gmail.com>
2025-07-11 21:37:52 -0300

    modulo adicionarAluno

---
+++ src/modulos/adicionarAluno.js
@@ -0,0 +1,36 @@
+export function adicionarAluno(corpoTabela, nome, frequencia, media, situacao, botaoRemover) {
+    limparCorpoTabela();
+
+    const novoAluno = document.createElement('tr');
+    corpoTabela.appendChild(novoAluno);
+
+    const nomeNovoAluno = document.createElement('td');
+    nomeNovoAluno.textContent = nome.value;
+    novoAluno.appendChild(nomeNovoAluno);
+
+    const frequenciaNovoAluno = document.createElement('td');
+    frequenciaNovoAluno.textContent = frequencia;
+    novoAluno.appendChild(frequenciaNovoAluno);
+
+    const mediaNovoAluno = document.createElement('td');
+    mediaNovoAluno.textContent = media;
+    novoAluno.appendChild(mediaNovoAluno);
+
+    const situacaoNovoAluno = document.createElement('td');
+    situacaoNovoAluno.textContent = situacao;
+    novoAluno.appendChild(situacaoNovoAluno);
+
+    const botaoRemoverAluno = document.createElement('td');
+    botaoRemoverAluno.innerHTML = botaoRemover;
+    novoAluno.appendChild(botaoRemoverAluno);
+};
+
+export function criarBotaoRemover() {
+    const botaoRemover = document.createElement('button');
+    botaoRemover.textContent = 'Remover';
+    return botaoRemover;
+};
+
+export function limparCorpoTabela(tbody) {
+    tbody.textContent = '';
+}
