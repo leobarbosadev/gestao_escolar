fe110cfae161bd7d5b9b4948cddb05ae46e3b9df
Gilber-Mattos-Mendes <ggmattosjf@gmail.com>
2025-07-14 21:04:14 -0300

    Validação de Campo Vazio

--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
-const nome = document.getElementById('nome_completo');
-const frequencia = document.getElementById('frequencia');
-const nota1 = Number(document.getElementById('nota1'));
-const nota2 = Number(document.getElementById('nota2'));
-const nota3 = Number(document.getElementById('nota3'));
-const nota4 = Number(document.getElementById('nota4'));
+import { adicionarEventoBotao } from "./utils/validacao.js";
+import { validar_entrada } from "./utils/validacao.js"
 
+function aoClicarAdicionar() {
+  const nome = document.getElementById('nome_completo').value.trim();
+  const frequencia = document.getElementById('frequencia').value.trim();
+  const nota1 = document.getElementById('nota1').value.trim();
+  const nota2 = document.getElementById('nota2').value.trim();
+  const nota3 = document.getElementById('nota3').value.trim();
+  const nota4 = document.getElementById('nota4').value.trim();
+
+validar_entrada(nome, frequencia, nota1, nota2, nota3, nota4);
+}
+
+adicionarEventoBotao('btn-adicionar', aoClicarAdicionar);
--- src/modulos/calcularMedia.js
+++ src/modulos/calcularMedia.js
@@ -1,4 +1,4 @@
-export function calcularMedia(n1,n2,n3,n4){
-        const soma = (n1+n2+n3+n4) / 4;
+export function calcularMedia(nota1,nota2,nota3,nota4){
+        const soma = (nota1+nota2+nota3+nota4) / 4;
     return (soma).toFixed(2);
 }
