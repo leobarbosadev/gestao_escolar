71466e3f3d6c78242dee9902dd55130ca19b6b48
colynascimento <colymsnascimento@gmail.com>
2025-07-14 21:26:01 -0300

    Main - removendo aluno na lista

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import { adicionarAluno } from "./modulos/adicionarAluno";
 import { criarBotaoRemover } from "./modulos/adicionarAluno";
 import { limparMensagemTabelaVazia } from "./modulos/adicionarAluno";
 import { adicionarEventoBotao } from "./utils/validacao.js";
+import { removerAluno } from "./modulos/removerAluno.js";
 import { validar_entrada } from "./utils/validacao.js"
 
 function aoClicarAdicionar() {
@@ -13,17 +14,21 @@ function aoClicarAdicionar() {
   const nota2 = document.getElementById('nota2').value.trim();
   const nota3 = document.getElementById('nota3').value.trim();
   const nota4 = document.getElementById('nota4').value.trim();
+  const corpoTabela = document.getElementById('corpo-tabela');
 
   const media = calcularMedia(nota1, nota2, nota3, nota4);
-const situacao = situacaoAluno(frequencia, media);
+  const situacao = situacaoAluno(frequencia, media);
 
-validar_entrada(nome, frequencia, nota1, nota2, nota3, nota4);
-
-document.getElementById('btn-adicionar').addEventListener('click', adicionarAluno(corpoTabela, nome, frequencia, media, situacao));
-
-
-
-}
+  validar_entrada(nome, frequencia, nota1, nota2, nota3, nota4);
+  adicionarAluno(corpoTabela, nome, frequencia, media, situacao);
+};
 
 adicionarEventoBotao('btn-adicionar', aoClicarAdicionar);
 
+function aoClicarRemover(event) {
+    const corpoTabela = document.getElementById('corpo-tabela');
+    
+    removerAluno(event, corpoTabela);
+};
+
+adicionarEventoBotao('botaoRemover', aoClicarRemover);
